1f4167afb9dc0939ada80c5c90fb7be14a75df20
Saurabh <qa@proptia.com>
2026-04-09 04:38:51 -0700

    fix(ci): suppress remaining cacheExtent deprecation warning

--- lib/src/internals/mixins.dart
+++ lib/src/internals/mixins.dart
@@ -101,6 +101,7 @@ mixin RefresherSliverComposer on State<SmartRefresher> {
 
       if (childView is ScrollView) {
         primary = primary ?? childView.primary;
+        // ignore: deprecated_member_use
         cacheExtent = cacheExtent ?? childView.cacheExtent;
         key = key ?? childView.key;
         semanticChildCount = semanticChildCount ?? childView.semanticChildCount;
